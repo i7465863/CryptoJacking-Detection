835548ed7375d15c07a4c91f437c79d93d238d46
i7465863 <50464385+i7465863@users.noreply.github.com>
2019-05-16 01:55:01 +0100

    comment code

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -1,4 +1,7 @@
+//popup.js provides dynamic updates to the popup window to cange element depending on the state of the current tab
 var blacklistBtn = document.getElementById('btnBlacklistToggle');
+var helpBtn = document.getElementById('btnHelp');
+//send blacklist message of current url if blacklist button is clicked
 blacklistBtn.onclick = function () {
 	chrome.tabs.query({
 		currentWindow: true,
@@ -7,7 +10,7 @@ blacklistBtn.onclick = function () {
 			var currentURL = new URL(tabs[0].url);
 			var domain = currentURL.hostname;
 			chrome.runtime.sendMessage(domain);
-		
+			//update blacklist button text depending on current url state
 			if (document.getElementById("btnBlacklistToggle").innerHTML == "Blacklist this website"){
 				document.getElementById("btnBlacklistToggle").innerHTML = "Remove from blacklist";
 			}
@@ -18,8 +21,15 @@ blacklistBtn.onclick = function () {
 	);
 	window.close();
 };
+//load help.html on help button click
+helpBtn.onclick = function () {
+	var newURL = "../html/help.html";
+	chrome.tabs.create({
+		url: newURL
+	});
+};
 
-
+//retrieve the status of the current tab to update the status text and blacklist button
 chrome.storage.local.get(['blockStatus'], function(result) {
 		
 	if (result.blockStatus == "Site Clear") {
@@ -32,15 +42,9 @@ chrome.storage.local.get(['blockStatus'], function(result) {
 	}
 });
 
-var helpBtn = document.getElementById('btnHelp');
-helpBtn.onclick = function () {
-	var newURL = "../html/help.html";
-	chrome.tabs.create({
-		url: newURL
-	});
-};
 
 
+//update status text on high CPU usage detection
 chrome.storage.local.get(['suspiciousSite'], function(result) {
 	chrome.tabs.query({
 		currentWindow: true,
